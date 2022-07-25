06b92b5d5ddb101ecd3c67f30ae51fcc30c8fb74
Champion Ogunsina <fimihan@Champions-MacBook-Pro.local>
2022-07-25 11:24:11 +0100

    mongoose and graphql schemas

---
+++ server/models/Client.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+const ClientSchema = mongoose.Schema({
+  name: { type: String },
+  email: { type: String },
+  phone: { type: String },
+});
+
+module.exports = mongoose.model("Client", ClientSchema);
