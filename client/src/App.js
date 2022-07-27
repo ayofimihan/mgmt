dd3dfff708280a9f2036e615f70147c8781f2d96
ayofimihan <cogunsina@gmail.com>
2022-07-27 13:02:39 +0100

    frontend barebones

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="container">
+      <Header/>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
